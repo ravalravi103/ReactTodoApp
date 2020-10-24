f67e8e2e1c851a2cc6252bd957815969e86a528b
ravalravi103 <ravalravi103@gmail.com>
2020-10-24 13:38:12 +0530

    folders and Files Created

---
+++ src/Component/TodoList/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TodoItem from '../TodoItem/TodoItem';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+               
+            </div>
+        )
+    }
+}
+
+
+export default TodoList;
